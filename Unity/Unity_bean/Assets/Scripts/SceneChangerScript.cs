b51baaad6fc81c352b45ffadbc2e876c5a06f4d5
Bogdans <bogdans.cumaks@sk.lvt.lv>
2026-02-04 09:38:29 +0200

    Jauns dators

---
+++ Unity/Unity_bean/Assets/Scripts/SceneChangerScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangerScript : MonoBehaviour
+{
+    public int sceneNumber;
+    public void Transition()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
